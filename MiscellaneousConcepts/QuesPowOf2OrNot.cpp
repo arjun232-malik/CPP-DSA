b77755cc000d279694df11d991bbb90c4972194d
arjun232-malik <arjunsiddhumalik@gmail.com>
2026-02-16 18:02:21 +0530

    Update QuesPowOf2OrNot.cpp

--- MiscellaneousConcepts/QuesPowOf2OrNot.cpp
+++ MiscellaneousConcepts/QuesPowOf2OrNot.cpp
@@ -1,13 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-    // int n=4;
-    // if((2<<1)==n){
-    //     cout<<"True"<<endl;
-    // }else{
-    //     cout<<"False"<<endl;
-    // }
-
     int n;
     cout<<"Enter the number:";
     cin>>n;
