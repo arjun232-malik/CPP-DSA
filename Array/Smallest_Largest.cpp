18938e110aad1b28dd6b4b085db38d12c3dc8820
arjun232-malik <arjunsiddhumalik@gmail.com>
2026-02-21 20:22:14 +0530

    C++

--- Array/Smallest_Largest.cpp
+++ Array/Smallest_Largest.cpp
@@ -3,10 +3,14 @@ using namespace std;
 int main(){
     int marks[5]={45,67,23,76,56};
     int sz=sizeof(marks)/sizeof(int);
+    int smallest=INT8_MAX;
 
     for(int i=0;i<sz;i++){
-        
+        if(marks[i]<smallest){
+            smallest=marks[i];
+        }
     }
+    cout<<smallest<<endl;;
 
     return 0;
 }
