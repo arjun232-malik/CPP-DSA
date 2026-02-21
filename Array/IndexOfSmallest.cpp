03428a0994dd7ea3feacb245faeb06f28a89aad9
arjun232-malik <arjunsiddhumalik@gmail.com>
2026-02-21 23:03:03 +0530

    C++

---
+++ Array/IndexOfSmallest.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+int main(){
+    int num[5]={45,8,23,-6,90};
+    int sz=sizeof(num)/sizeof(int);
+
+    // int smallest=INT8_MAX;
+    int largest=INT8_MIN;
+
+    for(int i=0;i<sz;i++){
+        // if(num[i]<smallest){
+        //     smallest=i;
+        // }
+
+        if(num[i]>largest){
+            largest=i;
+        }
+    }
+     std::cout<<largest<<"\n";
+    return 0;
+}
--- Array/Smallest_Largest.cpp
+++ Array/Smallest_Largest.cpp
@@ -1,16 +1,24 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int marks[5]={45,67,23,76,56};
+    int marks[5]={45,67,23,76,-42};
     int sz=sizeof(marks)/sizeof(int);
-    int smallest=INT8_MAX;
+    int smallest=INT8_MAX; // INT8_MAX ---> + infinity
+    int largest=INT8_MIN; // INT8_MAX ---> - infinity
 
-    for(int i=0;i<sz;i++){
-        if(marks[i]<smallest){
-            smallest=marks[i];
+    for(int i=0;i<sz;i++){ // min,max
+        // if(marks[i]<smallest){
+        //     smallest=marks[i];
+        // }
+        // smallest=min(marks[i],smallest);
+
+        if(marks[i]>largest){
+            largest=marks[i];
         }
+        // largest=max(marks[i],largest);
     }
-    cout<<smallest<<endl;;
+    // cout<<"The smallest:"<<smallest<<endl;
+    cout<<"The largest:"<<largest<<endl;
 
     return 0;
 }
