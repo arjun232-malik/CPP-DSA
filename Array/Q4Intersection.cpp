cc535612f8b167bcd023705d0ff59d6fdbefcab2
arjun232-malik <arjunsiddhumalik@gmail.com>
2026-02-24 22:48:05 +0530

    C++

---
+++ Array/Q4Intersection.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void intersection(int num1[],int num[],int sz);
+
+int main(){
+    int num1[]={45,32,21,67,91};
+    int num2[]={63,21,54,19,76};
+    int sz=sizeof(num1)/sizeof(int);
+
+    intersection(num1,num2,sz);
+
+    return 0;
+}
+
+void intersection(int num1[],int num[],int sz){
+
+    for(int i=0;i<sz;i++){
+        for(int j=0;j<=i;j++){
+            if(num1[i]==num[j]){
+                cout<<num[j]<<endl;
+                break;
+            }
+        }
+    }
+}
