44705feaedfb250c490da4bba411fde9725b6b14
arjun232-malik <arjunsiddhumalik@gmail.com>
2026-03-11 23:26:09 +0530

    C++

--- LeetCodeQues/Q2TwoSum.cpp
+++ LeetCodeQues/Q2TwoSum.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 int main(){
     // TRY TO SOLVE FROM STUDIED YET(3 MARCH 2026)
+    // BRUTE FORCE APPROACH
     vector<int> nums={3,3};
     int target=6;
     int sz=nums.size();
---
+++ LeetCodeQues/Q8RemovedDuplicate.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> nums={1,1,2};
+        int k=nums.size();
+        int i=0;
+        
+        for(;i<k-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums.erase(nums.begin()+i);
+                i--;
+                k++;
+            }
+        }
+        cout<<k<<endl;
+
+    return 0;
+}
